3c7ff650a095406c8e50a17460d861f1aa241b48
Ioannis Antoniou <johnantony24@hotmail.com>
2024-05-18 21:06:41 +0200

    migrations

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -5,3 +5,6 @@ class Posts(models.Model):
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
