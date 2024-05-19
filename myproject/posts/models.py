b974c89d306713c24814fd0538f7da7be0ed7dfa
Ioannis Antoniou <johnantony24@hotmail.com>
2024-05-19 21:10:28 +0200

    models

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -8,3 +8,12 @@ class Posts(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comments(models.Model):
+    title  = models.CharField(max_length=100)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
